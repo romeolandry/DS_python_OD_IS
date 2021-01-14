d68c1034ad2fcacf98d2ccf4955c3a16b0256e05
romeolandry <kamgoche@yahoo.com>
2021-01-14 23:35:41 +0100

    create main pipeline

--- configurations/configuration.py
+++ configurations/configuration.py
@@ -4,18 +4,18 @@ import os
 CUR_DIR = os.path.abspath(os.curdir)
 
 # INPUT
-CSI_INPUT = "/dev/video0" # Path to CSI- Camera eg. rasbery
-CAMERA_WIDTH=1920
-CAMERA_HEIGHT=1080
-BATCH_SIZE=1
-BATCH_PUSH_TIMEOUT= 4000000 # Default is 4000000
+CSI_INPUT = "/dev/video0"  # Path to CSI- Camera eg. rasbery
+CAMERA_WIDTH = 1920
+CAMERA_HEIGHT = 1080
+BATCH_SIZE = 1
+BATCH_PUSH_TIMEOUT = 4000000  # Default is 4000000
 
 # OUTPUT RTSP Configuration
-CODEC = "H264" # RTSP Streaming Codec H264/H265
-BITRATE = 4000000 #  Set the encoding bitrate. Type INT
+CODEC = "H264"  # RTSP Streaming Codec H264/H265
+BITRATE = 4000000  # Set the encoding bitrate. Type INT
 RTSP_PORT = 8554
-UDP_CONF ={
-    'host':'224.224.255.255',
+UDP_CONF = {
+    'host': '224.224.255.255',
     'port': 5400,
     'async': False,
     'sync': 1
@@ -23,22 +23,27 @@ UDP_CONF ={
 
 
 # MODEL CONFIGURATION
-AVAILABLE_TRT_MODEL = ['resnet10','custom']
+AVAILABLE_TRT_MODEL = ['resnet10', 'custom']
 AVAILABLE_TRITIS_MODEL = ['yolov3', 'ssd_inceptionv2', 'custom']
 
 # SSD Model configurtion
 
-## tensorflow
-CONFIG_SSD_INCEPTIONV2_COCO = "configurations/dstest_ssd_nopostprocess.txt"
-UNTRACKED_OBJECT_ID = 0xffffffffffffffff
-IMAGE_HEIGHT = 1080
-IMAGE_WIDTH = 1920
-MIN_BOX_WIDTH = 32
-MIN_BOX_HEIGHT = 32
-TOP_K = 20
-IOU_THRESHOLD = 0.3
+# tensorflow
+Model_CONF = {
+    'config_file': "configurations/dstest_ssd_nopostprocess.txt",
+    'untracted_object_id': 0xffffffffffffffff,
+    'img_height': 1080,
+    'img_width': 1920,
+    'min_box_width': 32,
+    'min_box_height': 32,
+    'top_k': 20,
+    'iou_threshold': 0.3
+}
+
 
 # Dataset
-CLASS_NB = 91
-ACCURACY_ALL_CLASS = 0.5
-COCO_LABEL_PATH = "Models/tritis_model/ssd_inception_v2_coco_2018_01_28/labels.txt"
+DATA_CONF = {
+    'nb_classes': 91,
+    'accuracy_all_class': 0.5,
+    'patht_to_label': "Models/tritis_model/ssd_inception_v2_coco_2018_01_28/labels.txt"
+}
--- python_app/pipeline_src.py
+++ python_app/pipeline_src.py
@@ -16,7 +16,6 @@ gi.require_version("Gst","1.0")
 from gi.repository import Gst
 
 
-
 def cis_camera_source(path_camera):
     print(f"Creating  Source for  and CIS Camera wiht the path {path_camera}")
 
@@ -44,7 +43,7 @@ def cis_camera_source(path_camera):
 
     print("elements for source was created and setted!")
 
-    return [source_cis,nvvidconv_src_cis,caps_nvvidconv_src_cis]
+    return [source_cis, nvvidconv_src_cis, caps_nvvidconv_src_cis]
 
 
 
